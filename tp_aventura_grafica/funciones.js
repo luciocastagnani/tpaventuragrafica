ee0042ad2ce9496b2f3e4ddd6a8d63eaabe76846
luciocastagnani <167034860+luciocastagnani@users.noreply.github.com>
2024-10-08 17:15:05 -0300

    Add files via upload

--- tp_aventura_grafica/funciones.js
+++ tp_aventura_grafica/funciones.js
@@ -6,9 +6,10 @@ function drawButton() {
   textAlign(CENTER, CENTER);
   text('Siguiente', width - 70, height - 40);
 }
+
 function mostrarPantallaConCursor() {
   fill(255);
   textSize(16);
   textAlign(LEFT, TOP);
-  text(`Pantalla ${pantallaqueestoy + 1}`, mouseX + 10, mouseY + 10); // Mostrar cerca del cursor
+  text(`Pantalla ${pantallaqueestoy + 1}`, mouseX + 10, mouseY + 10);
 }
--- tp_aventura_grafica/tp_aventura_grafica.js
+++ tp_aventura_grafica/tp_aventura_grafica.js
@@ -1,5 +1,9 @@
 let pantallaqueestoy = 0;
 const totaldepantallas = 25;
+let img1;
+function preload() {
+  img1 = loadImage('data/narutopantalla1.jpeg'); // Cargar la imagen para la pantalla 1
+}
 
 function setup() {
   createCanvas(640, 480);
@@ -7,12 +11,19 @@ function setup() {
 
 function draw() {
   background(150);
-  drawButton();
-  mostrarPantallaConCursor();
+  drawScreen(); 
+  drawButton(); 
+  mostrarPantallaConCursor(); // Mostrar el número de pantalla junto al cursor
+}
+
+function drawScreen() {
+  if (pantallaqueestoy === 0) {
+    image(img1, 0, 0, width, height); 
+}
 }
 
 function mousePressed() {
   if (mouseX > width - 120 && mouseX < width - 20 && mouseY > height - 60 && mouseY < height - 20) {
-    pantallaqueestoy = (pantallaqueestoy + 1) % totaldepantallas;
+    pantallaqueestoy = (pantallaqueestoy + 1) % totaldepantallas; // Avanzar a la siguiente pantalla
   }
 }
